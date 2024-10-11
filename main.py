62dd0ef7e7bccd39dbc0102fd151048a4044d868
curibe11 <curibe11@students.avc.edu>
2024-10-11 15:28:53 -0700

    L45-2

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ import random
 def main():
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-
-    print(f'The random values are {numbers}')
-    print(f'The total is {total}')
+    for i in range(5):
+       numbers.append(random.randint(0,100))
+    print(numbers[i], end = ' ')
+    total += numbers[i] 
+    print (total)
 
     ########################################
     # Do not delete the return statement
